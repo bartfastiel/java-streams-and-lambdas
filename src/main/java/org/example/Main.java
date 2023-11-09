0c26a60e2435e43cc5204a512c2387d8a3e56756
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-11-09 12:03:29 +0100

    Add video example

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,20 @@
 package org.example;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<String> fruits = List.of("Apple", "Banana", "Grapes", "Orange", "Mango");
+
+//        for (String fruit : fruits) {
+//            if (fruit.contains("p")) {
+//                System.out.println(fruit);
+//            }
+//        }
+
+        fruits.stream()
+                .filter(aFruit -> aFruit.contains("p"))
+                .forEach(System.out::println);
     }
 }
